94a190a0b32704c152b151590a0f362ac2cbd8fc
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2024-04-11 15:01:56 +0000

    change dir structure

--- src/p99Bluesky/beamlines/p99.py
+++ src/p99Bluesky/beamlines/p99.py
@@ -1,27 +1,24 @@
-"""
 from dodal.beamlines.beamline_utils import device_instantiation
 from dodal.beamlines.beamline_utils import set_beamline as set_utils_beamline
-from dodal.devices.turbo_slit import TurboSlit
 from dodal.log import set_beamline as set_log_beamline
 from dodal.utils import get_beamline_name
 
-BL = get_beamline_name("i20_1")
+from p99Bluesky.devices.p99.sample_stage import SampleStage
+
+BL = get_beamline_name("p99")
 set_log_beamline(BL)
 set_utils_beamline(BL)
 
 
-def turbo_slit(
+def sample_Stage(
     wait_for_connection: bool = True, fake_with_ophyd_sim: bool = False
-) -> TurboSlit:
-
-    turboslit for selecting energy from the polychromator
-
+) -> SampleStage:
+    """Sample stage for p99"""
 
     return device_instantiation(
-        TurboSlit,
+        SampleStage,
         prefix="-OP-PCHRO-01:TS:",
         name="turbo_slit",
         wait=wait_for_connection,
         fake=fake_with_ophyd_sim,
     )
-"""
---
+++ src/p99Bluesky/devices/p99/sample_stage.py
@@ -0,0 +1,9 @@
+from ..stages import PitchRollStage, SelectableStage, ThetaStage, XYZRealwVirStage
+
+
+class SampleStage(ThetaStage, PitchRollStage, XYZRealwVirStage, SelectableStage):
+    def __init__(self, prefix: str, name: str):
+        ThetaStage.__init__(self, prefix=prefix, name=name)
+        PitchRollStage.__init__(self, prefix=prefix, name=name)
+        XYZRealwVirStage.__init__(self, prefix=prefix, name=name, infix="Lab:")
+        SelectableStage.__init__(self, prefix=prefix, name=name)
