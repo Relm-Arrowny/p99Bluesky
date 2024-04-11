01218917a02c33bd142c99cf63ebb92ddbe1c0ac
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2024-04-11 09:26:40 +0000

    created p99 stage

--- src/p99Bluesky/devices/stages.py
+++ src/p99Bluesky/devices/stages.py
@@ -4,39 +4,42 @@ from ophyd_async.epics.motion.motor import Motor
 
 class XYZStage(Device):
     def __init__(self, prefix: str, name: str):
-        self.x = Motor(prefix=prefix + "X")
-        self.y = Motor(prefix=prefix + "Y")
-        self.z = Motor(prefix=prefix + "Z")
-        super().__init__(name=name)
+        self.x = Motor(prefix + "X")
+        self.y = Motor(prefix + "Y")
+        self.z = Motor(prefix + "Z")
+        Device.__init__(self, name=name)
 
 
 class PitchRollStage(Device):
     def __init__(self, prefix: str, name: str):
-        self.pitch = Motor(prefix=prefix + "WRITEPITCH")
-        self.roll = Motor(prefix=prefix + "WRITEROLL")
-        super().__init__(name=name)
+        self.pitch = Motor(prefix + "WRITEPITCH")
+        self.roll = Motor(prefix + "WRITEROLL")
+        Device.__init__(self, name=name)
 
 
 class SelectableStage(Device):
     def __init__(self, prefix: str, name: str):
-        self.select = Motor(prefix=prefix + "MP:SELECT")
-        super().__init__(name=name)
+        self.select = Motor(prefix + "MP:SELECT")
+        Device.__init__(self, name=name)
 
 
-class thetaStage(Device):
+class ThetaStage(Device):
     def __init__(self, prefix: str, name: str):
-        self.select = Motor(prefix=prefix + "WRITETHETA")
-        super().__init__(name=name)
+        self.theta = Motor(prefix + "WRITETHETA")
+        Device.__init__(self, name=name)
 
 
 class XYZRealwVirStage(XYZStage):
+    def __init__(self, prefix: str, name: str, infix: str):
+        self.virtualx = Motor(prefix + infix + "X")
+        self.virtualy = Motor(prefix + infix + "Y")
+        self.virtualz = Motor(prefix + infix + "Z")
+        XYZStage.__init__(self, prefix=prefix, name=name)
+
+
+class p99SampleStage(ThetaStage, PitchRollStage, XYZRealwVirStage, SelectableStage):
     def __init__(self, prefix: str, name: str):
-        self.virtualx = Motor(prefix=prefix + "Lab:X")
-        self.virtualy = Motor(prefix=prefix + "Lab:Y")
-        self.virtualz = Motor(prefix=prefix + "Lab:Z")
-        super().__init__(prefix=prefix, name=name)
-
-
-class p99SampleStage(XYZRealwVirStage, thetaStage, PitchRollStage, SelectableStage):
-    def __int__(self, prefix: str, name: str):
-        super().__init__(prefix=prefix, name=name)
+        ThetaStage.__init__(self, prefix=prefix, name=name)
+        PitchRollStage.__init__(self, prefix=prefix, name=name)
+        XYZRealwVirStage.__init__(self, prefix=prefix, name=name, infix="Lab:")
+        SelectableStage.__init__(self, prefix=prefix, name=name)
