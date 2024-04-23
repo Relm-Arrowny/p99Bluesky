cc4bec38b3126ad4c3db2779218a89ff96c9c558
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2024-04-23 09:54:54 +0000

    added tests for all three stages, add blueapi definiteion and FilterMotor

--- src/p99Bluesky/devices/p99/sample_stage.py
+++ src/p99Bluesky/devices/p99/sample_stage.py
@@ -1,4 +1,7 @@
+from enum import Enum
+
 from ophyd_async.core import Device
+from ophyd_async.epics.signal import epics_signal_rw
 
 from p99Bluesky.devices.epics.setReadOnlyMotor import SetReadOnlyMotor
 
@@ -14,4 +17,29 @@ class SampleAngleStage(Device):
         self.pitch = SetReadOnlyMotor(
             prefix, name, suffix=["WRITETHETA", "WRITETHETA:RBV", "WRITETHETA.EGU"]
         )
+        super().__init__(name=name)
+
+
+class p99StageSelections(str, Enum):
+    Empty = ("Empty",)
+    Mn5um = ("Mn 5um",)
+    Fe = ("Fe (empty)",)
+    Co5um = ("Co 5um",)
+    Ni5um = ("Ni 5um",)
+    Cu5um = ("Cu 5um",)
+    Zn5um = ("Zn 5um",)
+    Zr = ("Zr (empty)",)
+    Mo = ("Mo (empty)",)
+    Rh = ("Rh (empty)",)
+    Pd = ("Pd (empty)",)
+    Ag = ("Ag (empty)",)
+    Cd25um = ("Cd 25um",)
+    W = ("W (empty)",)
+    Pt = ("Pt (empty)",)
+    User = ("User",)
+
+
+class FilterMotor(Device):
+    def __init__(self, prefix: str, name: str):
+        self.user_setpoint = epics_signal_rw(p99StageSelections, prefix)
         super().__init__(name=name)
