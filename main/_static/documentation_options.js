3671b61bb44c1eac00b933d77df0b285fb8bdca9
Relm-Arrowny <Relm-Arrowny@users.noreply.github.com>
2024-05-07 21:06:27 +0000

    deploy: d0586dd5e7d3fc989fe7f769d9cef6a793241d08

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.0.4.dev32+ge6cb61a',
+    VERSION: '0.0.0.4.dev35+gd0586dd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
